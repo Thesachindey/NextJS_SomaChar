2538e897d1265c2edb2b1f826354c50861d0b80a
Sachin <sachinscode24@gmail.com>
2026-05-08 22:16:17 +0600

    impliment dynamic APIs: {searchParams}props, import  cookies() from next/heders, & use force-dynamic, force-static, error, auto

--- src/app/books/page.jsx
+++ src/app/books/page.jsx
@@ -1,16 +1,20 @@
-import React from 'react';
+//dynamically rendered static page
+import { cookies } from "next/headers";
+import React from "react";
 
-// export const dynamic='force-dynamic'; // this will force the page to be dynamic and will not generate static page for it.
+// export const dynamic='force-static';
+// export const dynamic='error';
+// export const dynamic='auto';
 
+// export const dynamic='force-dynamic';
+// this will force the page to be dynamic and will not generate static page for it.
 
-const Books = async ({searchParams}) => {
-    // await searchParams;
+const Books = async ({ searchParams }) => {
+  // await searchParams;
+  const myCookies = await cookies();
+  console.log(myCookies);
 
-    return (
-        <div>
-            this is the books page!
-        </div>
-    );
+  return <div>this is the books page!</div>;
 };
 
 export default Books;
