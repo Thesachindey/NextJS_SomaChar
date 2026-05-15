934a5e90668b46f2aa71a16f94ebfa5b14b00694
Sachin <sachinscode24@gmail.com>
2026-05-15 12:46:01 +0600

    added story page for render server component inside client component as a server component

---
+++ src/app/storys/page.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Story = () => {
+    return (
+        <div>
+            <h1>Story</h1>
+            <p>This is a simple story page.</p>
+        </div>
+    );
+};
+
+export default Story;
